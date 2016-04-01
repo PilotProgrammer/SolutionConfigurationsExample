91e9f669d6bcd44c568ebad82978ba7b801a7be1
PilotProgrammer <twogsw@yahoo.com>
2016-03-31 19:04:37 -0500

    SolutionConfigurationsClassLibraryClient Solution Add

---
+++ SolutionConfigurationsClassLibraryClient/SolutionConfigurationsClassLibraryClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolutionConfigurationsClassLibrary;
+
+namespace SolutionConfigurationsClassLibraryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseConnection conn = new DatabaseConnection();
+            WebServiceClient ws = new WebServiceClient();
+
+            FancyCalculator calc = new FancyCalculator(conn, ws, true);
+            int result = calc.DoFancyStuffWithANumber(5, true);
+            Console.WriteLine(result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
